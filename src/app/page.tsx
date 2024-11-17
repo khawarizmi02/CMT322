bb0709f1572b9bc6b71ca9937ff743b33860cb8c
khawarizmi <khawarizmijefri02@gmail.com>
2024-11-17 22:07:07 +0800

    converting to react 18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
+"use client";
+import { useState, useEffect } from "react";
+import {
+  query,
+  collection,
+  getDoc,
+  QuerySnapshot,
+  doc,
+  getDocs,
+} from "firebase/firestore";
+
 import Image from "next/image";
+import { db } from "@/firebase/firebase";
+// import { set } from "date-fns";
 
 export default function Home() {
+  const [data, setData] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchDocs = async () => {
+      const q = query(collection(db, "User"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+        setData((prevData) => [...prevData, doc.data()]);
+      });
+    };
+    fetchDocs();
+  }, []);
+
   return (
     <div>
       <div>Hello</div>
+      <div>
+        {data.map((item) => (
+          <div>{item.email}</div>
+        ))}
+      </div>
     </div>
   );
 }
