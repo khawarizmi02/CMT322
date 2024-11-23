880a0241f6c122cdc88ce39fb736611e05a8d1dc
khawarizmi <khawarizmijefri02@gmail.com>
2024-11-23 19:36:52 +0800

    Refactor project structure and remove unused code

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
+
+export const metadata: Metadata = {
+  title: 'SUKAD@USM',
+  icons: {
+    icon: '/icon.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    // <SidebarProvider>
+    //   <AppSidebar />
+    //   <main>
+    //     <SidebarTrigger />
+    //     <ScrollArea className="h-[1000px]">{children}</ScrollArea>
+    //   </main>
+    // </SidebarProvider>
+
+    <html lang="en">
+      <body className={`antialiased`}>
+        <main className="flex flex-col items-center">{children}</main>
+      </body>
+    </html>
+  );
+}
