45c028a291d74819f9d491fe87c1f480c9317850
Irfanly <muhdirfan.zainal02@gmail.com>
2024-11-26 19:31:51 +0800

    Add sport detail page and update sports list with categories.

--- src/app/(pages)/organizer/sports/page.tsx
+++ src/app/(pages)/organizer/sports/page.tsx
@@ -1,18 +1,40 @@
+//SHow all sports
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+
+import { SportsListDetails } from '@/data/mock-sportslist';
+import { SportsListDetailsCopy, SportsListCopy } from '@/data/mock-sportslist-copy';
 
 const Sports: React.FC = () => {
-  const data = [
-    {
-      id: 1,
-      name: 'Football',
-      image: '',
-    },
-  ];
+
+  const router = useRouter();
+
+  const handleClick = (sport: string) => {
+    router.push(`/organizer/sports/${sport}`)
+    console.log("clicked")
+  };
+    
   return (
     <div>
       <div>Sport</div>
+        <div>
+          {SportsListDetailsCopy.map((sport) => (
+            // <div key={sport.id} onClick={() => handleClick(sport.title)}>
+                  // <img src={category.image} alt={sport.title} />
+                  // <div>{category.category}</div>
+            <div key={sport.id} onClick={() => handleClick(sport.title)}>
+              { sport.categories?.map(( category ) => (
+                <div>
+                  <img src={category.image} alt={sport.title} />
+                  <div>{category.category}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+      </div>
     </div>
   );
-};
+}
 
 export default Sports;
