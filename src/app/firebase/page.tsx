75c2989e17db6e2dcd25b17411cae9e671a7df3d
khawarizmi <khawarizmijefri02@gmail.com>
2024-12-29 18:35:07 +0800

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/app/firebase/page.tsx
+++ src/app/firebase/page.tsx
@@ -10,12 +10,12 @@ import { checkRole } from '@/utils/roles'
 
 // Add your Firebase config object
 const firebaseConfig = {
-  apiKey: "AIzaSyAR_EQgF08evK1THQ-2MFQh9y-482xpvtQ",
-  authDomain: "cmt322-7940a.firebaseapp.com",
-  projectId: "cmt322-7940a",
-  storageBucket: "cmt322-7940a.firebasestorage.app",
-  messagingSenderId: "69399093065",
-  appId: "1:69399093065:web:91961503d621a16f814498"
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Connect to your Firebase app
